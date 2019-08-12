afcbd36d094ba3a73db4cc840e4cfd96f00eda31
seldadg <seldadg@gmail.com>
2019-08-12 11:36:15 +0300

    React Native Components

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+export default App;
+
+
+
