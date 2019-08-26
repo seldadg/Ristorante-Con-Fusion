c8cfb42c22eabb71c8e077df029d4288d9c50e30
seldadg <seldadg@gmail.com>
2019-08-26 21:02:48 +0300

    Social Sharing.

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input, Button } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -61,12 +61,22 @@ function RenderDish(props) {
           ],
           { cancelable: false }
         );
-        else if(recognizeComment(gestureState))
+      else if (recognizeComment(gestureState))
         props.onCommentPress();
-      
+
       return true;
     }
-  })
+  });
+
+  const shareDish = (title, message, url) => {
+    Share.share({
+      title: title,
+      message: title + ': ' + message + ' ' + url,
+      url: url
+    }, {
+        dialogTitle: 'Share ' + title
+      })
+  };
 
   if (dish !== null) {
     return (
@@ -99,6 +109,14 @@ function RenderDish(props) {
               color='#512DA8'
               onPress={props.onCommentPress}
             />
+            <Icon
+              raised
+              reverse
+              name='share'
+              type='font-awesome'
+              color='#51D2A8'
+              onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} 
+              />
           </View>
         </Card>
       </Animatable.View>
